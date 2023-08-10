758875966f4aa0a8e08533267acf287e63ec4ce0
Muhammad Talha Zulfiqar <muhammadtalha4163@gmail.com>
2023-08-11 04:38:45 +0500

    Final Touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./app.css";
 import Swal from "sweetalert2";
-import {AiOutlineLoading} from "react-icons/ai";
+import { AiOutlineLoading, AiOutlineSetting } from "react-icons/ai";
+import BlurBackgroundComponent from "./Midst";
 
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: "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",
+  //apiKey:"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",
 });
 const openai = new OpenAIApi(configuration);
 
@@ -14,6 +16,15 @@ const App = () => {
   const [gptResponse, setGptResponse] = useState("");
   const [context, setContext] = useState("");
   const [Loading, setLoading] = useState(false);
+  const [Models, setModels] = useState("gpt-3.5-turbo-16k");
+  const [Menu, setMenu] = useState(false);
+  const [Auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("password") === "passwordispassword") {
+      setAuth(true);
+    }
+  }, []);
 
   const simulateGptResponse = async () => {
     if (
@@ -26,6 +37,7 @@ const App = () => {
         icon: "error",
         title: "Oops...",
         text: "Please Enter the Context of atleast 10 characters.",
+        timer: 2000,
       });
       return;
     }
@@ -35,7 +47,19 @@ const App = () => {
         icon: "error",
         title: "Oops...",
         text: "Please Enter the Prompt of atleast 10 characters.",
+        timer: 2000,
       });
+      return;
+    }
+
+    if (!Auth) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please Authenticate yourself First From the Setting Menu.",
+        timer: 2000,
+      });
+      setMenu(true);
       return;
     }
 
@@ -43,7 +67,7 @@ const App = () => {
     let resp = "";
     try {
       const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-16k",
+        model: Models,
         messages: [
           {
             role: "system",
@@ -60,12 +84,11 @@ const App = () => {
 
       resp = completion.data.choices[0].message.content;
     } catch (error) {
-      console.error("Error fetching GPT response:", error);
       Swal.fire({
         icon: "error",
-        title: "Oops...",
-        text: "Something went wrong. Please try again later.",
-        timer: 2000,
+        title: error.response.data.error.code,
+        text: error.response.data.error.message,
+        timer: 10000,
       });
     }
 
@@ -76,48 +99,73 @@ const App = () => {
 
   return (
     <div className="bg-blue-400 flex flex-col items-center  h-screen w-screen justify-around">
-      <div className="bg-gray-200 w-[80%] h-[50%]  border rounded-2xl ">
-        <textarea
-          style={{ resize: "none" }}
-          className="w-full h-full border-gray-400 rounded-2xl p-4 overflow-y-auto"
-          value={gptResponse}
-          placeholder="GPT Response will be shown here...n"
-          onChange={(e) => setGptResponse(e.target.value)}
-        ></textarea>
-      </div>
+      <button
+        className="absolute top-2 right-4"
+        onClick={() => {
+          setMenu(!Menu);
+          console.log(Models);
+        }}
+      >
+        <AiOutlineSetting className="text-2xl text-black" />
+      </button>
+      {Menu ? (
+        <>
+          <div className="bg-gray-200 border rounded-2xl ">
+            <BlurBackgroundComponent
+              setMenu={setMenu}
+              setModels={setModels}
+              Auth={Auth}
+              setAuth={setAuth}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          {" "}
+          <div className="bg-gray-200 w-[80%] h-[50%]  border rounded-2xl ">
+            <textarea
+              style={{ resize: "none" }}
+              className="w-full h-full border-gray-400 rounded-2xl p-4 overflow-y-auto"
+              value={gptResponse}
+              placeholder="GPT Response will be shown here..."
+              onChange={(e) => setGptResponse(e.target.value)}
+            ></textarea>
+          </div>
+          <div className=" bg-gray-200 w-[80%] h-[40%]  relative border rounded-2xl shadow-lg flex flex-col justify-between">
+            <textarea
+              className="w-full h-[60%] md:h-[80%] p-4 rounded-2xl border-gray-400 overflow-y-auto"
+              onChange={(e) => setContext(e.target.value)}
+              style={{ resize: "none" }}
+              placeholder="Enter the Context here..."
+              value={context}
+            ></textarea>
 
-      <div className=" bg-gray-200 w-[80%] h-[40%]  relative border rounded-2xl shadow-lg">
-        <textarea
-          className="w-full h-[80%] p-4 rounded-2xl border-gray-400 overflow-y-auto"
-          onChange={(e) => setPromptin(e.target.value)}
-          style={{ resize: "none" }}
-          
-          placeholder="Enter the Context here..."
-        ></textarea>
-
-        <div className="flex absolute bottom-0 mb-2 w-full ">
-          <input
-            type="text"
-            className="ml-2 message-input w-full  rounded border border-gray-700 px-4 py-2 hover:border-[#7f18ef]/40 text-black bg-white"
-            placeholder="Type your prompt here..."
-            onChange={(e) => setContext(e.target.value)}
-            value={context}
-          />
-          <button
-            className="send-button bg-blue-500 hover:bg-blue-800 text-white rounded px-4 py-2 ml-2 mr-2"
-            onClick={() => {
-              console.log("Clicked");
-              simulateGptResponse();
-            }}
-            disabled={Loading}
-          >
-            {
-              Loading ? <AiOutlineLoading className="animate-spin"/> : "Send"
-            }
-            
-          </button>
-        </div>
-      </div>
+            <div className="flex flex-col md:flex-row bottom-0 mb-2 w-full ">
+              <input
+                type="text"
+                className="message-input md:w-full  rounded border border-gray-700 hover:border-[#7f18ef]/40 text-black bg-white mb-4 md:mb-0 px-4 py-2 ml-2 mr-2"
+                placeholder="Type your prompt here..."
+                onChange={(e) => setPromptin(e.target.value)}
+                value={Promptin}
+              />
+              <button
+                className="send-button bg-blue-500 hover:bg-blue-800 text-white rounded px-4 py-2 ml-2 mr-2 items-center flex justify-center"
+                onClick={() => {
+                  console.log("Clicked");
+                  simulateGptResponse();
+                }}
+                disabled={Loading}
+              >
+                {Loading ? (
+                  <AiOutlineLoading className="animate-spin" />
+                ) : (
+                  "Send"
+                )}
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/Midst.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import { FaTimes } from "react-icons/fa"; // Import the close icon from React Icons
+
+const BlurBackgroundComponent = (props) => {
+  const [password, setPassword] = useState(
+    localStorage.getItem("password") || ""
+  );
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("password")  === "passwordispassword") {
+      props.setAuth(true);
+    }
+  }, []);
+
+  const handleAuthentication = () => {
+    if (password === "passwordispassword") {
+      props.setAuth(true);
+      localStorage.setItem("password", password);
+    } else {
+      alert("Incorrect password. Try again.");
+    }
+  };
+
+  const handleCloseClick = () => {
+    // Implement the close logic here
+    console.log("Close button clicked");
+    props.setMenu(false);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center rounded-2xl p-4 bg-white relative">
+
+
+        <div className="absolute inset-0 bg-blur-image blur"></div>
+        <div className="z-10 text-center ">
+            <button onClick={handleCloseClick}>
+                <FaTimes className="text-2xl text-black
+                "/>
+            </button>
+          {props.Auth ? (
+            <div className="p-4 m-4">
+              <h1 className="text-3xl font-semibold">props.Auth</h1>
+              <button className="text-xl font-semibold bg-red-200 rounded-2xl p-4 mt-4">
+                Remove Authentication
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-col md:flex-row">
+              <input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                placeholder="Password"
+                className="p-4 rounded-md shadow-md focus:outline-none m-4"
+              />
+              <button
+                onClick={handleAuthentication}
+                className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none m-4"
+              >
+                Authenticate
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-between relative">
+          <h1 className="bg-gray-300 p-4 m-2 font-bold rounded-2xl z-10">
+            Model
+          </h1>
+          <select className="bg-gray-300 rounded-2xl m-2 p-4 cursor-pointer z-20" onChange={(e)=>{
+            props.setModels(e.target.value);
+          }}>
+            <option value="gpt-3.5-turbo-16k">GPT-3.5 Turbo</option>
+            <option value="gpt-4">GPT-4</option>
+            <option value="gpt-4-32k">GPT 4 32K</option>
+          </select>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BlurBackgroundComponent;
